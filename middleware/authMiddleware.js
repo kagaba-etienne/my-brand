5709e7f5defe660740a4e7dfa576747e63ac3be6
kagaba-etienne <kagabaetienne04@gmail.com>
2023-03-20 00:18:23 +0200

    bg-commenting: fixing commenting

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -8,8 +8,7 @@ const requireAuth = (req, res, next) => {
     if (token) {
         jwt.verify(token, process.env.SECRET, (err, decodedToken) => {
             if (err) {
-                console.log(err.message)
-                console.log('hiii');
+                console.log(err.message);
                 res.redirect('/login');
             } else {
                 next();
@@ -23,7 +22,7 @@ const requireAuth = (req, res, next) => {
 
 //check current user
 
-const checkUser = (req, res, next) => {
+const checkUser = (req, res, next) => { 
     const token = req.cookies.jwt;
 
     if (token) {
