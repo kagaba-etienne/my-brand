2f6e112edfa598512a71472fb36ce4f0c13e667d
kagaba-etienne <kagabaetienne04@gmail.com>
2023-03-20 13:50:22 +0200

    ft-signup: protecting signup route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,10 @@ const auth = express.Router();
 const authController = require('../controllers/authController');
 const { requireAuth } = require('../middleware/api/authMiddleware');
 
-auth.get('/signup', authController.get_signup);
+auth.get('/signup', requireAuth, authController.get_signup);
 auth.post('/signup', requireAuth, authController.post_signup);
 auth.get('/login', authController.get_login);
-auth.get('/logout', authController.get_logout);
 auth.post('/login', authController.post_login);
+auth.get('/logout', authController.get_logout);
 
 module.exports = auth;
